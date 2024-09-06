5080ef3f5a7c36ae8bc3e3aaf52062ec272cc918
alpona <mstalponak61@gmail.com>
2024-09-06 23:24:48 +0600

    file change

--- comparison.js
+++ comparison.js
@@ -1,18 +1,18 @@
-// console.log( 5 < 10 );
-// console.log( 5 > 10 );
-// console.log( 5 == 10 );
-// console.log(5 >= 10 );
-// console.log( 10 >= 10 );
-// console.log( 5 <= 10 );
-// console.log( 10 <= 10);
-// var applePrice = 250;
-// var dagronPrice = 300;
+console.log( 5 < 10 );
+console.log( 5 > 10 );
+console.log( 5 == 10 );
+console.log(5 >= 10 );
+console.log( 10 >= 10 );
+console.log( 5 <= 10 );
+console.log( 10 <= 10);
+var applePrice = 250;
+var dagronPrice = 300;
 
-// console.log( applePrice < dagronPrice);
-//  ----------------------------------------------------
-//  console.log( 10 == '10');
-// console.log( 10 === '10');
-// console.log( 10 != '10');
+console.log( applePrice < dagronPrice);
+
+ console.log( 10 == '10');
+console.log( 10 === '10');
+console.log( 10 != '10');
 console.log( 5 != '10' );
 
 
