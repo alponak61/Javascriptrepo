38291223d12bf07b1dd9817f819617bce08f5b03
alpona <mstalponak61@gmail.com>
2024-09-07 00:06:01 +0600

    if-else condition

---
+++ ifelse.js
@@ -0,0 +1,22 @@
+// if( 10 > 6 )
+// {
+//     console.log("i buy earring");
+// }
+
+// var ricksha = 60;
+//  if( ricksha <= 40 ){
+//     console.log('i will go');
+//  }
+//  else{
+//     console.log('iam not going');
+//  }
+
+var chocolate = 300;
+if( chocolate < 100 )
+{
+    console.log('i will buy');
+
+}
+else{
+    console.log('iam not buying');
+}
