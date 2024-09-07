f99a9881901381d9e7a68848619be3485925ef3d
alpona <mstalponak61@gmail.com>
2024-09-07 15:58:29 +0600

    complex condition of if-else

--- ifelse.js
+++ ifelse.js
@@ -1,15 +1,15 @@
-// if( 10 > 6 )
-// {
-//     console.log("i buy earring");
-// }
+if( 10 > 6 )
+{
+    console.log("i buy earring");
+}
 
-// var ricksha = 60;
-//  if( ricksha <= 40 ){
-//     console.log('i will go');
-//  }
-//  else{
-//     console.log('iam not going');
-//  }
+var ricksha = 60;
+ if( ricksha <= 40 ){
+    console.log('i will go');
+ }
+ else{
+    console.log('iam not going');
+ }
 
 var chocolate = 300;
 if( chocolate < 100 )
@@ -19,4 +19,30 @@ if( chocolate < 100 )
 }
 else{
     console.log('iam not buying');
+}
+
+ var  salary = 4000;
+ var hight = 75;
+ var isBcas = true ;
+
+if( salary >  3000 || hight < 80 ){
+    console.log('jamai tumi aso');
+}
+else{
+    console.log('bago tumi mukbul');
+}
+
+
+if( salary > 8000 && hight <30 ){
+    console.log('new jamai');
+}
+else{
+    console.log(' old jamai ');
+}
+
+if((salary < 5000 || hight > 80 ) && (salary > 2000 || isBcas == false )){
+    console.log('ai vai ai');
+}
+else{
+    console.log('kono dorkar nai');
 }
