884fc7cfd9675585d82056d98c00e2c8f4b9d94d
alpona <mstalponak61@gmail.com>
2024-09-11 00:04:44 +0600

    push pop shift unshift

--- array.js
+++ array.js
@@ -1,8 +1,8 @@
 const number = [1, 2, 3, 4, 5, 6];
-// console.log(number);
-// console.log(number[4]);
-// const fifth = number [2];
-// console.log(fifth);
+console.log(number);
+console.log(number[4]);
+const fifth = number [2];
+console.log(fifth);
 let abc = 20;
 abc = 40;
 number [4] = 204;
---
+++ push-pop.js
@@ -0,0 +1,19 @@
+let number = [ 1, 2, 3, 4, 5, 6];
+console.log(number);
+number.push(22);
+console.log(number);
+let out1 = number.pop();
+console.log(number);
+let out2 = number.pop();
+console.log(number);
+
+number.shift(23);
+console.log(number);
+number.unshift(9);
+console.log(number);
+
+let friend =['ailam', 'gelam', 'jailam', 'khaylam'];
+console.log(friend);
+friend.push('billal');
+console.log(friend);
+
