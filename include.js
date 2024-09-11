e7b75c6b54ba04e49c869d120f8fd492b885ef39
alpona <mstalponak61@gmail.com>
2024-09-11 23:26:36 +0600

    include

---
+++ include.js
@@ -0,0 +1,24 @@
+let friend =['ailam', 'gelam', 'jailam', 'khaylam'];
+// console.log(friend.includes('ailam'));
+// console.log(friend.includes('suy'));
+//  if(friend.includes('jailam')){
+//     console.log('party');
+//  }
+//  else{
+//     console.log('jilapi');
+//  }
+
+// console.log(friend.indexOf('khaylam'));
+// console.log(friend.indexOf('khay'));
+
+// let number =[];
+// let food = 'jdjhadk hjadhja aa';
+// console.log(Array.isArray(friend));
+// console.log(Array.isArray(number));
+// console.log(Array.isArray(food));
+
+// console.log(friend.join('+'));
+let number = [ 1, 2, 3, 4, 5];
+console.log(friend.concat(number));
+
+
---
+++ loop.js
@@ -0,0 +1,51 @@
+
+// let fruits = ['apple', 'orange', 'banana', 'jackfruit'];
+// console.log(fruits);
+// for( const fruit of fruits){
+//     console.log(fruit);
+//     console.log('i want to buy');
+// }
+
+// let number = [1, 2, 3, 4, 5];
+// console.log(number);
+// for(const num of number){
+//     console.log(num);
+//     console.log('calculte');
+// }
+
+// let num = 1;
+// if( num < 6){
+//     console.log('loop');
+// }
+// else{
+//  console.log('not loop');
+// }
+
+// let num = 0;
+// while( num < 5){
+//     console.log('looping', num);
+//     num = num + 1;
+// }
+
+// let num = 1;
+// sum = 0;
+// while( num < 10 )
+// {
+//     console.log(num);
+//     sum = sum + num;
+//     num++;
+// }
+// console.log('sum=', sum);
+
+let num = 1;
+while( num <= 10 ){
+    console.log(num);
+//   if( num % 2 === 0){
+//         console.log('even=', num);
+//     }
+  if( num % 2 !== 0 ){
+    console.log('odd=',num);
+  }
+    num++;
+
+}
